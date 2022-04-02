51891af81e62ae70284b2788d768a6b9fbe26d98
MRX1205 <2732184038@qq.com>
2022-04-02 22:29:31 +0800

    new class

--- emhlz/TestGit.java
+++ emhlz/TestGit.java
@@ -1,2 +1,9 @@
-package emhlz;public class TestGit {
+package emhlz;
+
+public class TestGit {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello Git");
+    }
 }
+
